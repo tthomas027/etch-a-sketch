f8dee46cd2b2ca9716d6511049f5be24999c667b
Travis Thomas <tthomas027@gmail.com>
2021-06-17 21:12:04 -0700

    Change divs in grid to black on mouseenter

--- scripts.js
+++ scripts.js
@@ -4,10 +4,17 @@ function createGrid() {
   for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
       const div = document.createElement("div");
-      div.classList.add('row' + i, 'grid');
+      div.classList.add('grid');
       container.appendChild(div);
     }
   }
 }
 
 createGrid();
+
+const divs = document.querySelectorAll('.grid');
+divs.forEach(div  => 
+  div.addEventListener('mouseenter', () => {
+    div.classList.add('black');
+  })
+)
